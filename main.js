a27a59d7b0c537f3a9d34836be7682519437f743
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-09 18:05:22 -0300

    now you can add and remove multiple obstacles

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 const mapContainer = document.querySelector(".mapContainer");
 
 let objective;
-let clickCounter;
+let obstacles = [];
+let obstaclesID = [];
 
 
 
@@ -12,6 +13,7 @@ class coordinates{
     constructor(){
         this.x;
         this.y;
+        this.ID;
     }
 }
 
@@ -20,15 +22,21 @@ mapContainer.addEventListener('mousedown', e => {
     mapClick(e.target.id)
 })
 
+
 function mapClick(tileID){
+    console.log("tileID=", tileID)
+    
     if(objective == undefined){
         defineObjective(tileID);
     }
     else if(tileID.match(/\d/g)[0] == objective.x && tileID.match(/\d/g)[1] == objective.y){
-        undefineObjective(tileID);
+        undefineObjective();
+    }
+    else if (obstaclesID.includes(tileID)){
+       undefineObstacles(tileID);
     }
     else{
-        console.log("Objective is already defined.")
+        defineObstacles(tileID);
     }
 }
 
@@ -37,11 +45,29 @@ function defineObjective(tileID){
     objective = new coordinates()
     objective.x = tileID.match(/\d/g)[0];
     objective.y = tileID.match(/\d/g)[1];
+    objective.ID = tileID;
     console.log("New Objective =", objective);
 }
 
-function undefineObjective(tileID){
+function undefineObjective(){
     document.getElementById("x"+objective.x+"y"+objective.y).style.background = "white";
     objective = undefined;
-    console.log("Objective Deleted");
+    console.log("Objective Removed");
+}
+
+function defineObstacles(tileID){
+    document.getElementById(tileID).style.background = "#ff7d7d";
+    obstacles.push(new coordinates());
+    obstacles[obstacles.length-1].x = tileID.match(/\d/g)[0];
+    obstacles[obstacles.length-1].y = tileID.match(/\d/g)[1];
+    obstacles[obstacles.length-1].ID = tileID;
+    obstaclesID.push(tileID);
+    console.log("New Obstacle =", obstacles[obstacles.length-1]);
+}
+
+function undefineObstacles(tileID){
+    document.getElementById(tileID).style.background = "white";
+    obstacles.splice(obstacles.findIndex(e=> e.ID == tileID), 1);
+    obstaclesID.splice(obstaclesID.indexOf(tileID), 1);
+    console.log("Obstacle Removed!")
 }
