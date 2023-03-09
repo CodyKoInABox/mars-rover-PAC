34c44bd641db3a43f9fa21ef42830d0c29876afb
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-09 16:10:44 -0300

    add coordinates constructor

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 
 const mapContainer = document.querySelector(".mapContainer");
 
+
+// constructor for coordinates object, useful to easily and quickly define coordinates
+class coordinates{
+    constructor(){
+        this.x;
+        this.y;
+    }
+}
+
+
 mapContainer.addEventListener('click', e => {
     console.log(e.target);
     console.log(e.target.id);
