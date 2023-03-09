1bf6eeae753c3c21f8ffa46a2ccf0658aa648cbc
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-09 00:03:41 -0300

    Add helper.js
    A file to help me write a lot of code without actually writing a lot of code, maybe this should be in .gitignore but I guess it might be useful to upload it here

---
+++ helper.js
@@ -0,0 +1,7 @@
+//file to help me write a lot of code without actually writing a lot of code
+
+for(let i=1; i<=8; i++){
+    for(let j=1; j<=8; j++){
+        console.log('<div class="mapTile" id="x'+j+'y'+i+'"> </div>');
+    }
+}
